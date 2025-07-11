aed686090d6c21533705c1cdc411852f9f490015
Matt Krueger <mkrueger@rstms.net>
2025-07-11 16:22:06 -0600

    Bump version: 0.0.4 → 0.0.5

--- cmd/root.go
+++ cmd/root.go
@@ -46,7 +46,7 @@ var ExitCode *int
 var vmx workstation.Controller
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.4",
+	Version: "0.0.5",
 	Use:     "vmx",
 	Short:   "control VMWare Workstation instances",
 	Long: `
