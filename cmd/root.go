a0bc431abcfb6ba277f29e7de4614b978ddb9a6b
Matt Krueger <mkrueger@rstms.net>
2025-07-22 03:14:54 -0600

    Bump version: 0.0.26 → 0.0.27

--- cmd/root.go
+++ cmd/root.go
@@ -54,7 +54,7 @@ const (
 var vmx ws.Controller
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.26",
+	Version: "0.0.27",
 	Use:     "vmx",
 	Short:   "control VMWare Workstation instances",
 	Long: `
--- ws/controller.go
+++ ws/controller.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const Version = "0.0.26"
+const Version = "0.0.27"
 
 var WINDOWS_ENV_PATTERN = regexp.MustCompile(`^WINDIR=.*WINDOWS.*`)
 
