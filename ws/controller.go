b3aa00bf5c206ee0fe52f3b9111b0deb2740a53d
Matt Krueger <mkrueger@rstms.net>
2025-07-27 09:08:00 -0600

    Bump version: 0.1.4 → 0.1.5

--- ws/controller.go
+++ ws/controller.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-const Version = "0.1.4"
+const Version = "0.1.5"
 
 var WINDOWS_ENV_PATTERN = regexp.MustCompile(`^WINDIR=.*WINDOWS.*`)
 var ENCRYPTED_VM_ERROR = regexp.MustCompile(`Something went wrong while getting password from stdin`)
