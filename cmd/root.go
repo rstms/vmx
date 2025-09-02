1019b93a2bcf251a0a59434ce9b9abe74ec976ba
Matt Krueger <mkrueger@rstms.net>
2025-09-02 03:34:24 -0600

    Bump version: 0.1.18 → 0.1.19

--- cmd/root.go
+++ cmd/root.go
@@ -52,7 +52,7 @@ const (
 var vmx ws.Controller
 
 var rootCmd = &cobra.Command{
-	Version: "0.1.18",
+	Version: "0.1.19",
 	Use:     "vmx",
 	Short:   "control VMWare Workstation instances",
 	Long: `
--- ws/controller.go
+++ ws/controller.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-const Version = "0.1.18"
+const Version = "0.1.19"
 
 var WINDOWS_ENV_PATTERN = regexp.MustCompile(`^WINDIR=.*WINDOWS.*`)
 var ENCRYPTED_VM_ERROR = regexp.MustCompile(`Something went wrong while getting password from stdin`)
