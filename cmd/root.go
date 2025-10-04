a9b79b3f360785e5ea9379b3b1c3c1d750ebac13
Matt Krueger <mkrueger@rstms.net>
2025-10-04 06:32:49 -0600

    Bump version: 0.1.22 → 0.1.23

--- cmd/root.go
+++ cmd/root.go
@@ -52,7 +52,7 @@ const (
 var vmx ws.Controller
 
 var rootCmd = &cobra.Command{
-	Version: "0.1.22",
+	Version: "0.1.23",
 	Use:     "vmx",
 	Short:   "control VMWare Workstation instances",
 	Long: `
